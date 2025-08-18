70efd0d10e7469d3408fa334f7610272249d208d
Jarred Rudolph <jarred.rudolph@gmail.com>
2025-08-19 00:10:27 +0800

    Angry at myself for not committing

--- Java/Week 4/DSAQueue.java
+++ Java/Week 4/DSAQueue.java
@@ -1,4 +1,150 @@
 class DSAQueue {
-    
-    
+    /* Varialbles */
+    protected int DEFAULT_CAPACITY = 100;
+    protected Object queue[];
+    protected int count = 0;
+
+    /* Default Constructor */
+    public DSAQueue()
+    {
+        queue = new Object[DEFAULT_CAPACITY];
+    }
+
+    /* Alternate Constructor */
+    public DSAQueue(int maxCapacity)
+    {
+        queue = new Object[maxCapacity];
+    }
+
+    /* Accessor Methods */
+    public int getCount()
+    {
+        return count;
+    }
+
+    public Boolean isEmpty()
+    {
+        return (count == 0);
+    }
+
+    public Boolean isFull()
+    {
+        return (count == queue.length);
+    }
+
+}
+
+class DSAShuffleQueue extends DSAQueue {
+
+    /* Default Constructor */
+    public DSAShuffleQueue()
+    {
+        super();
+    }
+
+    /* Alternate Constructor */
+    public DSAShuffleQueue(int maxCapacity)
+    {
+        super(maxCapacity);
+    }
+
+    /* Accessor Methods */
+    public Object peek()
+    {
+        Object topVal;
+        if (isEmpty())
+        {
+            throw new IllegalArgumentException("Error! The queue is empty!");
+        } else
+        {
+            topVal = this.queue[0];
+        }
+        return topVal;
+    }
+
+    /* Mutator Methods */
+    public void enqueue(Object value)
+    {
+        if (isFull())
+        {
+            throw new IllegalArgumentException("Error! The queue is full!");
+        } else
+        {
+            queue[count] = value;
+            count++;
+        }
+    }
+
+    public Object dequeue()
+    {
+        Object topVal = peek();
+
+        for (int ii = 1; ii < count; ii++)
+        {
+            queue[ii - 1] = queue[ii];
+        }
+
+        count--;
+        return topVal;
+    }
+
+}
+
+class DSACircularQueue extends DSAQueue {
+
+    /* Variables */
+    private int front = 0;
+    private int back = 0;
+    private final int maxCapacity;
+
+    /* Default Constructor */
+    public DSACircularQueue()
+    {
+        super();
+        maxCapacity = DEFAULT_CAPACITY;
+    }
+
+    /* Alternate Constructor */
+    public DSACircularQueue(int maxCapacity)
+    {
+        super(maxCapacity);
+        this.maxCapacity = maxCapacity;
+    }
+
+    /* Accessor Methods */
+    public Object peek()
+    {
+        Object topVal;
+        if (isEmpty())
+        {
+            throw new IllegalArgumentException("Error! The queue is empty!");
+        } else
+        {
+            topVal = queue[front];
+        }
+        return topVal;
+    }
+
+    /* Mutator Methods */
+    public void enqueue(Object value)
+    {
+        if (isFull())
+        {
+            throw new IllegalArgumentException("Error! The queue is full!");
+        } else
+        {
+            back = (back + 1) % maxCapacity;
+            queue[back] = value;
+            count++;
+        }
+    }
+
+    public Object dequeue()
+    {
+        Object topVal = peek();
+        front = (front + 1) % maxCapacity;
+        count--;
+        return topVal;
+    }
+
 }
